2bb45ed882e1278b6731d390ae70cd9cf2ffe3af
Kiongo-Bob <njoguub@gmail.com>
2023-02-21 18:20:31 -0800

    Alphabet 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: always (0) success
+ *
+ */
+
+vois rint_alphabet_x10(void)
+{
+	int num;
+	int ch;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+
+		_putchar('\n')
+	}
+}
