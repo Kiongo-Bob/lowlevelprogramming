6191462cf314315bd143b1008abb63e50944d622
Kiongo-Bob <njoguub@gmail.com>
2023-02-21 18:01:05 -0800

    Add main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
  * main - The main function
  * description - Prints putchar
- * Returns: (0) success
+ * Returns: always (0) success
  */
  
  int main(void)
  {
- 	_putchar('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
