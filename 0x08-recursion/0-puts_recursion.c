4b95fce0a56b936fa277e9bea47543b309598bcf
Kiongo-Bob <Njoguub@gmail.com>
2023-03-08 19:37:55 +0300

    Fxn to print str followed by \n

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string follwed by anew line.
+ * pointer to char
+ * No return.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
