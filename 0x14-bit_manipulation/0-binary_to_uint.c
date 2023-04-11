02417b76e8885ec04513ae7b47f2f2cb172b1605
Kiongo-Bob <Njoguub@gmail.com>
2023-04-11 22:04:59 +0300

    Return unsigned

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+/**
+ * binary_to_uint - convert a binary number to an
+ * unsigned int.
+ * @b: binary.
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+    if (b == NULL)
+        return 0;
+
+    unsigned int result = 0;
+    int bit = 1;
+
+    for (int i = strlen(b) - 1; i >= 0; i--)
+    {
+        if (b[i] == '1')
+            result += bit;
+
+        if (b[i] != '0' && b[i] != '1')
+            return 0;
+
+        bit *= 2;
+    }
+
+    return result;
+}
+
