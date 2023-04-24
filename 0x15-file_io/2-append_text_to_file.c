01a580575fea492289afc82adefa587b834140a0
Kiongo-Bob <Njoguub@gmail.com>
2023-04-24 10:48:58 +0300

    Fxn that appends end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,38 +1,39 @@
 #include "main.h"
 
 /**
- * append_txt_2_file - Appends text at the end of a file
- * @filename: Filename.
- * @text_content: Additional content.
- * Return: 1 if the file exists else return -1 if inexistent or if it fails.
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename.
+ * @text_content: added content.
+ *
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
  */
-
-int append_txt_2_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_descriptor;
-	int txt_cont;
-	int bytes_count;
+	int fd;
+	int len = 0;
+	int written;
 
 	if (!filename)
 		return (-1);
 
-	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (file_descriptor == -1)
+	if (fd == -1)
 		return (-1);
 
 	if (text_content)
 	{
-		for (txt_cont = 0; text_content[txt_cont]; txt_cont++)
-			;
+		while (text_content[len])
+			len++;
 
-		bytes_count = write(file_descriptor, text_content, txt_cont);
+		written = write(fd, text_content, len);
 
-		if (bytes_count == -1)
+		if (written == -1)
 			return (-1);
 	}
 
-	close(file_descriptor);
+	close(fd);
 
 	return (1);
 }
