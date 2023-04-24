8882450bf4eb2273a2e257bfaf16aff7b0143a2f
Kiongo-Bob <Njoguub@gmail.com>
2023-04-24 10:21:59 +0300

    Fxn that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,34 +7,30 @@
  *
  * Return: 1 if it is successful else returns -1.
  */
+
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    int n, r;
-
     if (!filename)
-        return (-1);
+        return -1;
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    FILE *f = fopen(filename, "w");
 
-    if (fd == -1)
-        return (-1);
+    if (!f)
+        return -1;
 
-    if (!text_content)
-        text_content = "";
-
-    n = strlen(text_content);
-
-    r = write(fd, text_content, n);
-
-    if (r == -1)
+    if (text_content)
     {
-        close(fd);
-        return (-1);
+        size_t n = fwrite(text_content, 1, strlen(text_content), f);
+
+        if (n != strlen(text_content))
+        {
+            fclose(f);
+            return -1;
+        }
     }
 
-    close(fd);
+    fclose(f);
 
-    return (1);
+    return 1;
 }
 
