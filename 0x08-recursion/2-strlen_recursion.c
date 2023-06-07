2f0b8dfa4006a46dc738a990f0203008f5a3b3d0
Kiongo-Bob <njoguub@gmail.com>
2023-06-08 00:07:06 +0300

    No loops

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,16 @@
-#include "main.h"
-
 /**
- * _strlen_recursion: a function that returns the length of a string
+ * _strlen_recursion - a function that returns the length of a string
  * @s: string
  * Return: length
-*/
+ */
 int _strlen_recursion(char *s)
 {
-    int length = 0;
-    while (*s != '\0')
+    if (*s == '\0')
     {
-        length++;
-        s++;
+        return 0;
     }
-    return length;
+    else
+    {
+        return 1 + _strlen_recursion(s + 1);
+    }
 }
-
