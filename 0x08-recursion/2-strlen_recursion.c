47c88d7333e8c00569a50d70530b8d904f1521b0
Kiongo-Bob <njoguub@gmail.com>
2023-06-08 00:15:54 +0300

    Betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,16 @@
 /**
- * _strlen_recursion - a function that returns the length of a string
- * @s: string
- * Return: length
- */
+* _strlen_recursion - a function that returns the length of a string
+* @s: string
+* Return: length
+*/
 int _strlen_recursion(char *s)
 {
     if (*s == '\0')
     {
-        return 0;
+        return (0);
     }
     else
     {
-        return 1 + _strlen_recursion(s + 1);
+        return (1) + _strlen_recursion(s + 1);
     }
 }
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,15 +10,15 @@ int _pow_recursion(int x, int y)
 {
     if (y < 0)
     {
-        return -1; // Invalid input, power is undefined for negative exponents
+        return -1;
     }
     else if (y == 0)
     {
-        return 1; // Base case: any number raised to the power of 0 is 1
+        return 1;
     }
     else
     {
-        return x * _pow_recursion(x, y - 1); // Recursive call: multiply x with power of (x, y - 1)
+        return x * _pow_recursion(x, y - 1);
     }
 }
 
